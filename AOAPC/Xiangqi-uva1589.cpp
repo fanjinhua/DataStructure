a47c7343aebaed448d8ea6ed7e4b99a0999b2467
jfanner <fanyiyia@gmail.com>
2017-04-05 23:14:56 +0800

    uva1589

--- AOAPC/Xiangqi-uva1589.cpp
+++ AOAPC/Xiangqi-uva1589.cpp
@@ -1,316 +1,103 @@
 /**/
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <set>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+const int N = 12;
 
-struct Pos
+char brd[N][N];
+int dx[] = { -1, 1, 0, 0 }, dy[] = { 0, 0, -1, 1 };
+int hx[] = { -2, -1, -2, -1, 1, 2, 1, 2 };  //马将军
+int hy[] = { -1, -2, 1, 2, -2, -1, 2, 1 };
+int tx[] = { -1, -1, -1, -1, 1, 1, 1, 1 };  //马蹩脚
+int ty[] = { -1, -1, 1, 1, -1, -1, 1, 1 };
+int cr[2], cc[2];  //记录炮的坐标
+
+int check(int r, int c)
 {
-	Pos(int y, int x) : y_(y), x_(x) {}
-	int y_;
-	int x_;
-};
+	int i, j, k, tr, tc, cnt;
+	if (r < 1 || r > 3 || c < 4 || c > 6) return 1;
 
-struct Piece
-{
-	Piece() : p_(0, 0) {}
-	char name_;
-	Pos p_;
-};
+	for (j = c - 1; j > 0; --j)   //行被车
+	{
+		if (brd[r][j])
+			if (brd[r][j] == 'R') return 1;
+			else break;
+	}
+	for (j = c + 1; j <= 9; ++j)
+	{
+		if (brd[r][j])
+			if (brd[r][j] == 'R') return 1;
+			else break;
+	}
 
-Piece general;
-std::vector<Piece> white(10);
+	for (i = r - 1; i > 0; --i)  //列被车
+	{
+		if (brd[i][c])
+			if (brd[i][c] == 'R') return 1;
+			else break;
+	}
+	for (i = r + 1; i <= 10; ++i) //列被车或将
+	{
+		if (brd[i][c])
+			if (brd[i][c] == 'R' || brd[i][c] == 'G') return 1;
+			else break;
+	}
 
-std::vector<Pos> threat_pos(Piece p)
-{
-	std::vector<Pos> ps;
-	//std::set<Pos> area;
-	//for (int y = 1; y <= 3; ++y)
-		//for (int x = 4; x <= 6; ++x)
-			//area.insert(Pos(y, x));
+	for (int k = 0; k < 2; ++k)    //被炮将军
+	{
+		if (cr[k] == r)   //行有炮
+		{
+			for (j = c - 1, cnt = 0; j > cc[k]; --j) if (brd[r][j]) ++cnt;
+			if (cnt == 1) return 1;
+			for (j = c + 1, cnt = 0; j < cc[k]; ++j) if (brd[r][j]) ++cnt;
+			if (cnt == 1) return 1;
+		}
+		if (cc[k] == c)   //列有炮
+		{
+			for (i = r - 1, cnt = 0; i > cr[k]; --i) if (brd[i][c]) ++cnt;
+			if (cnt == 1) return 1;
+			for (i = r + 1, cnt = 0; i < cr[k]; ++i) if (brd[i][c]) ++cnt;
+			if (cnt == 1) return 1;
+		}
+	}
 
-	switch (p.name_)
+	for (int k = 0; k < 8; ++k)   //被马将军
 	{
-	case 'G':
-		ps.push_back(Pos(1, p.p_.x_));
-		ps.push_back(Pos(2, p.p_.x_));
-		ps.push_back(Pos(3, p.p_.x_));
-		break;
-	case 'R':
-		if (p.p_.x_ >= 4 && p.p_.x_ <= 6 && p.p_.y_ >= 1 && p.p_.y_ <= 3)
-		{
-			if (p.p_.x_ == 4 && p.p_.y_ == 1)
-			{
-				ps.push_back(Pos(1, 5));
-				ps.push_back(Pos(1, 6));
-				ps.push_back(Pos(2, 4));
-				ps.push_back(Pos(3, 4));
-			}
-			else if (p.p_.x_ == 5 && p.p_.y_ == 1)
-			{
-				ps.push_back(Pos(1, 4));
-				ps.push_back(Pos(1, 6));
-				ps.push_back(Pos(2, 5));
-				ps.push_back(Pos(3, 5));
-			}
-			else if (p.p_.x_ == 6 && p.p_.y_ == 1)
-			{
-				ps.push_back(Pos(1, 5));
-				ps.push_back(Pos(1, 4));
-				ps.push_back(Pos(2, 6));
-				ps.push_back(Pos(3, 6));
-			}
-			else if (p.p_.x_ == 6 && p.p_.y_ == 2)
-			{
-				ps.push_back(Pos(1, 6));
-				ps.push_back(Pos(3, 6));
-				ps.push_back(Pos(2, 5));
-				ps.push_back(Pos(2, 4));
-			}
-			else if (p.p_.x_ == 6 && p.p_.y_ == 3)
-			{
-				ps.push_back(Pos(3, 4));
-				ps.push_back(Pos(3, 5));
-				ps.push_back(Pos(2, 6));
-				ps.push_back(Pos(1, 6));
-			}
-			else if (p.p_.x_ == 5 && p.p_.y_ == 3)
-			{
-				ps.push_back(Pos(1, 5));
-				ps.push_back(Pos(2, 5));
-				ps.push_back(Pos(3, 4));
-				ps.push_back(Pos(3, 6));
-			}
-			else if (p.p_.x_ == 4 && p.p_.y_ == 3)
-			{
-				ps.push_back(Pos(3, 5));
-				ps.push_back(Pos(3, 6));
-				ps.push_back(Pos(2, 4));
-				ps.push_back(Pos(1, 4));
-			}
-			else if (p.p_.x_ == 4 && p.p_.y_ == 2)
-			{
-				ps.push_back(Pos(1, 4));
-				ps.push_back(Pos(2, 5));
-				ps.push_back(Pos(2, 6));
-				ps.push_back(Pos(3, 4));
-			}
-			else
-			{
-				ps.push_back(Pos(1, 5));
-				ps.push_back(Pos(2, 4));
-				ps.push_back(Pos(2, 6));
-				ps.push_back(Pos(3, 5));
-			}
-		}
-		else if (p.p_.x_ >= 4 && p.p_.x_ <= 6)
-		{
-			ps.push_back(Pos(1, p.p_.x_));
-			ps.push_back(Pos(2, p.p_.x_));
-			ps.push_back(Pos(3, p.p_.x_));
-		}
-		else if (p.p_.y_ >= 1 && p.p_.y_ <= 3)
-		{
-			ps.push_back(Pos(p.p_.y_, 4));
-			ps.push_back(Pos(p.p_.y_, 5));
-			ps.push_back(Pos(p.p_.y_, 6));
-		}
-		break;
-	case 'C':
-		if (p.p_.x_ >= 4 && p.p_.x_ <= 6)
-		{
-			for (auto& f : white)
-			{
-				if (f.p_.x_ == p.p_.x_ && f.p_.y_ <= p.p_.y_)
-				{
-					int i;
-					if (f.p_.y_ <= 3)
-						i = f.p_.y_ - 1;
-					else
-						i = 3;
-					while (i-- > 0)
-						ps.push_back(Pos(i, p.p_.x_));
-				}
-			}
-		  
-		}
-		else if (p.p_.y_ >= 1 && p.p_.y_ <= 3)
-		{
-			if (p.p_.x_ <= 4)
-			{
-				for (auto& f : white)
-				{
-					if (f.p_.y_ == p.p_.y_ && f.p_.x_ > p.p_.x_)
-					{
-						int i;
-						if (f.p_.x_ >= 4)
-							i = f.p_.x_ + 1;
-						else
-							i = 4;
-						for (; i <= 6; ++i)
-							ps.push_back(Pos(p.p_.y_, i));
-					}
-				}
-			}
-			else if (p.p_.x_ >= 6)
-			{
-				for (auto& f : white)
-				{
-					if (f.p_.y_ == p.p_.y_ && f.p_.x_ < p.p_.x_)
-					{
-						int i;
-						if (f.p_.x_ <= 6)
-							i = f.p_.x_ - 1;
-						else
-							i = 6;
-						for (; i >= 4; --i)
-							ps.push_back(Pos(p.p_.y_, i));
-					}
-				}
-			}
-		}
-		break;
-	case 'H':
-		std::vector<Pos> d;
-		bool hob_o = 0, hob_b = 0, hob_l = 0, hob_r = 0;
-		for (auto& f : white)
-		{
-			if (f.p_.x_ == p.p_.x_)
-			{
-				if (f.p_.y_ == p.p_.y_ + 1)
-					hob_o = 1;
-				if (f.p_.y_ == p.p_.y_ - 1)
-					hob_b = 1;
-			}
-			if (f.p_.y_ == p.p_.y_)
-			{
-				if (f.p_.x_ == p.p_.x_ + 1)
-					hob_r = 1;
-				if (f.p_.x_ == p.p_.x_ - 1)
-					hob_l = 1;
-			}
-		}
-		if (!hob_o && p.p_.y_ >= 3)
-		{
-			ps.push_back(Pos(p.p_.y_ - 2, p.p_.x_ - 1));
-			ps.push_back(Pos(p.p_.y_ - 2, p.p_.x_ + 1));
-		}
-		if (!hob_b && p.p_.y_ <= 8)
-		{
-			ps.push_back(Pos(p.p_.y_ + 2, p.p_.x_ - 1));
-			ps.push_back(Pos(p.p_.y_ + 2, p.p_.x_ + 1));
-		}
-		if (!hob_l && p.p_.x_ >= 3)
-		{
-			ps.push_back(Pos(p.p_.y_ - 1, p.p_.x_ - 2));
-			ps.push_back(Pos(p.p_.y_ + 1, p.p_.x_ - 2));
-		}
-		if (!hob_r && p.p_.x_ <= 7)
-		{
-			ps.push_back(Pos(p.p_.y_ - 1, p.p_.x_ + 2));
-			ps.push_back(Pos(p.p_.y_ + 1, p.p_.x_ + 2));
-		}
-		break;
+		tr = r + hx[k], tc = c + hy[k];
+		if (tr < 1 || tr > 10 || tc < 1 || tc > 9) continue;
+		if (brd[tr][tc] == 'H' && (!brd[r + tx[k]][c + ty[k]]))
+			return 1;
 	}
-	return ps;
-}
 
-std::vector<Pos> can_move_to(Piece p)
-{
-	std::vector<Pos> ps;
-	if (p.p_.x_ == 4 && p.p_.y_ == 1)
-	{
-		ps.push_back(Pos(1, 5));
-		ps.push_back(Pos(2, 4));
-	}
-	else if (p.p_.x_ == 5 && p.p_.y_ == 1)
-	{
-		ps.push_back(Pos(1, 4));
-		ps.push_back(Pos(1, 6));
-		ps.push_back(Pos(2, 5));
-	}
-	else if (p.p_.x_ == 6 && p.p_.y_ == 1)
-	{
-		ps.push_back(Pos(1, 5));
-		ps.push_back(Pos(2, 6));
-	}
-	else if (p.p_.x_ == 6 && p.p_.y_ == 2)
-	{
-		ps.push_back(Pos(1, 6));
-		ps.push_back(Pos(3, 6));
-		ps.push_back(Pos(2, 5));
-	}
-	else if (p.p_.x_ == 6 && p.p_.y_ == 3)
-	{
-		ps.push_back(Pos(3, 5));
-		ps.push_back(Pos(2, 6));
-	}
-	else if (p.p_.x_ == 5 && p.p_.y_ == 3)
-	{
-		ps.push_back(Pos(2, 5));
-		ps.push_back(Pos(3, 4));
-		ps.push_back(Pos(3, 6));
-	}
-	else if (p.p_.x_ == 4 && p.p_.y_ == 3)
-	{
-		ps.push_back(Pos(3, 5));
-		ps.push_back(Pos(2, 4));
-	}
-	else if (p.p_.x_ == 4 && p.p_.y_ == 2)
-	{
-		ps.push_back(Pos(1, 4));
-		ps.push_back(Pos(2, 5));
-		ps.push_back(Pos(3, 4));
-	}
-	else 
-	{
-		ps.push_back(Pos(1, 5));
-		ps.push_back(Pos(2, 4));
-		ps.push_back(Pos(2, 6));
-		ps.push_back(Pos(3, 5));
-	}
-	return ps;
+	return 0;
 }
 
 int main()
 {
-	int n;
-	const char* debug = "1 4\n";
-	const char* debug2 = "G 10 5\nR 6 4\n";
-
-	n = 2;
-	if (n) //while (sscanf(debug, "%d", &n) == 1 && n)
+	char s[5];
+	int n, r, c, x, y;
+	while (scanf("%d%d%d", &n, &r, &c), n || r || c)
 	{
-		sscanf(debug, "%d%d", &general.p_.y_, &general.p_.x_);
+		memset(brd, 0, sizeof(brd));
+		cr[0] = cc[0] = cr[1] = cc[1] = 0;
+
 		while (n--)
 		{
-			Piece p;
-			scanf(debug2, "%c %d %d", &p.name_, &p.p_.y_, &p.p_.x_);
-			white.push_back(p);
-		}
-		std::vector<Pos> esc = can_move_to(general);
-		std::vector<Pos> threat;
-		for (auto f : white)
-		{
-			std::vector<Pos> t = threat_pos(f);
-			threat.insert(threat.end(), t.begin(), t.end());
-		}
-		bool checkmate = 1;
-		for (auto e = esc.begin(); e != esc.end(); ++e)
-			for (auto t = threat.begin(); t != threat.end(); ++t)
+			scanf("%s%d%d", s, &x, &y);
+			if (s[0] == 'C')
 			{
-				if (e->x_ == t->x_ && e->y_ == t->y_)
-				{
-					e->x_ = e->y_ = 0;
-					break;
-				}
+				if (cr[0]) cr[1] = x, cc[1] = y;
+				else cr[0] = x, cc[0] = y;
 			}
-		for (auto& f : esc)
-			if (f.x_ != 0 && f.y_ != 0)
-				checkmate = 0;
-		checkmate ? printf("YES\n") : printf("NO\n");
-	}
+			brd[x][y] = s[0];
+		}
 
+		int cnt = 0;
+		for (int i = 0; i < 4; ++i)
+			cnt += check(r + dx[i], c + dy[i]);
+		if (cnt < 4) puts("NO");
+		else puts("YES");
+	}
 	return 0;
 }
 /**/
