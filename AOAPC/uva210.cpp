75e4525a2d39136ef159b5adfaeace0d3323a422
jfanner <fanyiyia@gmail.com>
2017-04-19 14:48:03 +0800

    notice conctrol input for times

--- AOAPC/uva210.cpp
+++ AOAPC/uva210.cpp
@@ -36,7 +36,7 @@ void run(int pid)
 		}
 		else if (tokens.size() == 2)  // print
 		{
-			cout << pid + 1 << ": " << vars[tokens.back()] << "\n";
+			cout << pid + 1 << ": " << vars[tokens.back()] << endl;
 			quan -= t_print;
 		}
 		else if (tokens.size() == 1)
@@ -76,7 +76,7 @@ void run(int pid)
 int main()
 {
 	int T;
-	//ifstream fin("in");
+	ifstream fin("in");
 	cin >> T;
 	while (T--)
 	{
@@ -84,21 +84,21 @@ int main()
 		ip.clear();
 		vars.clear();
 		
-		cin >> n >> t_assign >> t_print >> t_lock >> t_unlock >> t_end >> quantum;
-		string stat;
+		fin >> n >> t_assign >> t_print >> t_lock >> t_unlock >> t_end >> quantum;
 		int line = 0;
-		ip.push_back(line);
-		while (getline(cin, stat))
-		{
-			prog.push_back(stat);
-			if (stat == "end")
-				ip.push_back(line + 1);
-			line++;
-		}
-		ip.pop_back();
 		for (int i = 0; i < n; ++i)
+		{
+			string stat;
+			ip.push_back(line);
+			while (stat != "end")
+			{
+				getline(fin, stat);
+				prog.push_back(stat);
+				line++;
+			}
 			ready.push_back(i);
-
+		}
+		
 		locked = false;
 		while (!ready.empty())
 		{
