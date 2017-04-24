61326a3f2a1aae6787658fae22f565e6a7ee4543
jfanner <fanyiyia@gmail.com>
2017-04-24 22:06:38 +0800

    da

--- DataStructure/Node.h
+++ DataStructure/Node.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stack>
+#include <queue>
 #define stature(p) ((p) ? (p)->height_ : -1)
 typedef enum { RB_RED, RB_BLACK } RBColort;
 
@@ -23,6 +24,29 @@ public:
 		default: break;
 		}
 	}
+	template<typename VST> void trav_in(VST& visit)
+	{
+		switch (rand() % 2)
+		{
+		case 0:
+			trav_in_i1(this, visit); break;
+		case 1:
+			trav_in_i2(this, visit); break;
+		case 2:
+			trav_in_r(this, visit); break;
+		}
+	}
+	template<typename VST> void trav_post(VST& visit)
+	{
+		switch (rand() % 3)
+		{
+		case 0:
+			trav_post_i1(this, visit); break;
+		case 1:
+			trav_post_r(this, visit); break;
+		}
+	}
+	template<typename VST> void trav_level(VST& visit);
 	T data_;
 	Node* parent_;
 	Node* left_;
@@ -61,5 +85,86 @@ void trav_pre_i(Node<T>* x, VST& visit)
 		if (s.empty()) break;
 		x = s.top();
 		s.pop();
+	}
+}
+
+template<typename T, typename VST>
+static void go_along_left_branch(Node<T>* x, std::stack<Node<T>*>& s)
+{
+	while (!x)
+	{
+		s.push(x);
+		x = x->left_;
+	}
+}
+
+template<typename T, typename VST>
+void trav_in_i1(Node<T>* x, VST& visit)
+{
+	std::stack<Node<T>*> s;
+	while (true)
+	{
+		go_along_left_branch(x, s);
+		if (s.empty()) break;
+		x = s.top(), s.pop();
+		visit(x->data_);
+		x = x->right_;
+	}
+}
+
+template<typename T, typename VST>
+void trav_in_i2(Node<T>* x, VST& visit)
+{
+	std::stack<Node<T>*> s;
+	while (true)
+	{
+		if (x)
+		{
+			s.push(x);
+			x = x->left_;
+		}
+		else if (!s.empty())
+		{
+			x = s.top(), s.pop();
+			visit(x->data_);
+			x = x->right_;
+		}
+		else
+			break;
+	}
+}
+
+template<typename T, typename VST>
+void trav_in_r(Node<T>* x, VST& visit)
+{
+	if (!x)
+		return;
+	trav_in_r(x->left_, visit);
+	visit(x->data_);
+	trav_in_r(x->right_, visit);
+}
+
+template<typename T, typename VST>
+void trav_post_r(Node<T>* x, VST& visit)
+{
+	if (!x)
+		return;
+	trav_post_r(x->left_, visit);
+	trav_post_r(x->right_, visit);
+	visit(x->data_);
+}
+
+template<typename T>
+template<typename VST>
+inline void Node<T>::trav_level(VST & visit)
+{
+	std::queue<Node<T>*> q;
+	q.push(this);
+	while (!q.empty())
+	{
+		Node<T>* x = q.top(), q.pop();
+		visit(x->data_);
+		if (x->left_) q.push(x->left_);
+		if (x->right_) q.push(x->right_);
 	}
 }
