7d9b178f3f5c08bee82be164974c13e79ee3b557
jfanner <fanyiyia@gmail.com>
2017-05-06 22:26:35 +0800

    tree

---
+++ AOAPC/uva699.cpp
@@ -0,0 +1,45 @@
+/** /
+#include <iostream>
+using namespace std;
+
+const int maxn = 200;
+int sum[maxn];
+
+void build(int p)
+{
+	int v; cin >> v;
+	if (v == -1) return;
+	sum[p] += v;
+	build(p - 1);
+	build(p + 1);
+}
+
+bool init()
+{
+	int v; cin >> v;
+	if (v == -1) return false;
+	memset(sum, 0, sizeof(sum));
+	int pos = maxn / 2;
+	sum[pos] = v;
+	build(pos - 1);
+	build(pos + 1);
+}
+
+int main()
+{
+	int kase = 0;
+	while (init())
+	{
+		int p = 0;
+		while (sum[p] == 0) p++;
+		
+		cout << "Case " << ++kase << ":\n" << sum[p++];
+		while (sum[p] != 0)
+		{
+			cout << " " << sum[p];
+			p++;
+		}
+		cout << "\n\n";
+	}
+}
+/**/
