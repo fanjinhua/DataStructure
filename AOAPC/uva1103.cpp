4f66b1889469e491ded04a26b3659d4170f4d57e
jfanner <fanyiyia@gmail.com>
2017-05-10 22:35:04 +0800

    todo

--- AOAPC/uva1103.cpp
+++ AOAPC/uva1103.cpp
@@ -1,12 +1,17 @@
-/**/
+/** /
 #include <cstdio>
+#include <cstring>
 const int maxh = 200 + 5, maxw = 50 + 5;
 int h, w;
+char matrix[maxh][maxw];
 
 int main()
 {
 	while (scanf("%d%w", &h, &w) == 2 && h && w)
 	{
+		memset(matrix, 0, sizeof(matrix));
+		for (int r = 0; r < h; ++r)
+			scanf("%s", matrix[r]);
 
 	}
 }
