a2992eef36f68ef2952482d2ff5541af2af6796b
jfanner <fanyiyia@gmail.com>
2017-05-13 22:51:27 +0800

    DFS

---
+++ AOAPC/uva10562.cpp
@@ -0,0 +1,25 @@
+/**/
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+using namespace std;
+
+const int maxn = 200 + 10;
+int n;
+char buf[maxn][maxn];
+
+void solve()
+{
+
+}
+
+int main()
+{
+	int T;
+	fgets(buf[0], maxn, stdin);
+	sscanf(buf[0], "%d", &T);
+	while (T--)
+		solve();
+	return 0;
+}
+/**/
