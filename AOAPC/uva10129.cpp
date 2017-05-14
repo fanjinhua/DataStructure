7597f39827959d8b386672ef75afc59aeffd730d
jfanner <fanyiyia@gmail.com>
2017-05-14 23:03:55 +0800

    union set

--- AOAPC/uva10129.cpp
+++ AOAPC/uva10129.cpp
@@ -1,3 +1,43 @@
-/**/
+/** /
+#include <cstdio>
+#include <cstring>
+const int maxn = 100000 + 5;
+const int vern = 26;
+int G[vern][vern];
+int in_dgr[vern];
+int o_dgr[vern];
 
+bool euler(int u)
+{
+	for (int v = 0; v < vern; ++v)
+	{
+		if (G[u][v])
+		{
+
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int T, N; 
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d", &N);
+		memset(G, 0, sizeof(G));
+		memset(in_dgr, 0, sizeof(in_dgr));
+		char* word;
+		while (N--)
+		{
+			scanf("%s", word);
+			int head = *word - 'a';
+			int tail = *(word + strlen(word) - 1) - 'a';
+			G[head][tail] = 1;
+		}
+
+		euler();
+	}
+}
 /**/
--- AOAPC/uva10562.cpp
+++ AOAPC/uva10562.cpp
@@ -10,7 +10,21 @@ char buf[maxn][maxn];
 
 void solve()
 {
+	n = 0;
+	for (;;)
+	{
+		fgets(buf[n], maxn, stdin);
+		if (buf[n][0] = '#')
+			break;
+		else
+			n++;
+	}
+	printf("(");
+	if (n)
+	{
+		for (int i = 0; i < strlen(buf[0]); i++)
 
+	}
 }
 
 int main()
