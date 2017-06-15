72c3e023e6b4da394ca90fc299a147de6c011897
jfanner <fanyiyia@gmail.com>
2017-06-15 18:06:37 +0800

    dfs

--- AOAPC/uva12166.cpp
+++ AOAPC/uva12166.cpp
@@ -1,18 +1,52 @@
 /**/
-#include<iostream>
-#include<algorithm>
-#include<map>
-#include<string>
+#include <algorithm>
+#include <map>
+#include <cstdio>
+#include <cstring>
 using namespace std;
-typedef long long ll;
 
-void dfs()
+char expr[1048576];
+map<long long, int> R;
+void dfs(int l, int r, int dep) 
 {
-
+	if (expr[l] == '[') 
+	{
+		int p = 0;
+		for (int i = l + 1; i <= r - 1; i++) 
+		{
+			if (expr[i] == '[')	
+				p++;
+			if (expr[i] == ']')	
+				p--;
+			if (p == 0 && expr[i] == ',') 
+			{
+				dfs(l + 1, i - 1, dep + 1);
+				dfs(i + 1, r - 1, dep + 1);
+			}
+		}
+	}
+	else 
+	{
+		int w;
+		expr[r + 1] = '\0';
+		sscanf(expr + l, "%d", &w);
+		R[(long long)w << dep]++;
+	}
 }
-
-int main()
-{
+int main() {
+	int testcase;
+	FILE* fin = fopen("in", "r");
+	fscanf(fin, "%d", &testcase);
+	while (testcase--) 
+	{
+		fscanf(fin, "%s", expr);
+		R.clear();
+		dfs(0, strlen(expr) - 1, 0);
+		int sum = 0, mx = 0;
+		for (map<long long, int>::iterator it = R.begin(); it != R.end(); it++)
+			sum += it->second, mx = max(mx, it->second);
+		printf("%d\n", sum - mx);
+	}
 	return 0;
 }
 /**/
