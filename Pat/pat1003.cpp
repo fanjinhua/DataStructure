3d31d0937f1f9db3c86f0be67d6f322e230afe88
jfanner <fanyiyia@gmail.com>
2017-06-24 23:06:16 +0800

    dijkstra

--- Pat/pat1003.cpp
+++ Pat/pat1003.cpp
@@ -1,68 +1,72 @@
 /**/
 #include <iostream>
-#include <cstring>
 #include <algorithm>
 #include <fstream>
 using namespace std;
-const int inf = 11111111;
+const int inf = 1000000000;
 const int maxn = 500 + 5;
-int w[maxn][maxn];
-int n, m, c1, c2;
-int teams[maxn], vis[maxn], dist[maxn], path[maxn];
- 
+int n, m, from, to;
+bool vis[maxn];
+int w[maxn][maxn], num[maxn], teams[maxn], dist[maxn], way[maxn];
+
+void dijkstra(int s) 
+{
+	for (int i = 0; i < n; ++i) 
+	{
+		dist[i] = inf;
+		vis[i] = false;
+		num[i] = way[i] = 0;
+	}
+	num[s] = teams[s];
+	way[s] = 1;
+	dist[s] = 0;
+	for (int j = 0; j < n; ++j) 
+	{
+		int k = -1;
+		int m = inf;
+		for (int i = 0; i < n; ++i) 
+			if ((!vis[i]) && (dist[i] <= m))
+				m = dist[k = i];
+
+		vis[k] = true;
+
+		for (int i = 0; i < n; ++i) 
+			if (dist[k] + w[k][i] < dist[i])
+			{
+				dist[i] = dist[k] + w[k][i];
+				num[i] = num[k] + teams[i];
+				way[i] = way[k];
+			}
+			else if (dist[k] + w[k][i] == dist[i])
+			{
+				num[i] = max(num[k] + teams[i], num[i]);
+				way[i] += way[k];
+			}
+
+	}
+}
+
 int main()
 {
 	ifstream fin("patin");
-	fin >> n >> m >> c1 >> c2;
-	memset(vis, 0, sizeof(vis));
+	fin >> n >> m >> from >> to;
 	
 	for (int i = 0; i < n; ++i)
 	{
-		dist[i] = inf;
-		teams[i] = 0;
-		path[i] = -1;
+		fin >> teams[i];
 		for (int j = 0; j < n; ++j)
 			w[i][j] = inf;
 	}
 
-	for (int i = 0; i < n; ++i) fin >> teams[i];
 	for (int i = 0; i < m; ++i)
 	{
-		int n1, n2;
-		fin >> n1 >> n2;
-		fin >> w[n1][n2];
+		int n1, n2, wight;
+		fin >> n1 >> n2 >> wight;
+		w[n1][n2] = w[n2][n1] = min(w[n1][n2], wight);
 	}
-	for (int i = 0; i < n; ++i)
-	{
-		dist[i] = w[c1][i];
-		if (dist[i] != inf)
-			path[i] = c1;
-	}
-	dist[c1] = 0;
-
-	for (int i = 0; i < n; ++i)
-	{
-		int x, smallest = inf;
-		for (int c = 0; c < n; ++c)
-			if (!vis[c] && dist[c] <= smallest)
-				smallest = dist[x = c];
-		vis[x] = 1;
-		for (int c = 0; c < n; ++c)
-			if (w[x][c] != inf)
-			{
-				dist[c] = min(dist[c], dist[x] + w[x][c]);
-				path[i] = x;
-			}
-	}
-
-	int tms = teams[c1];
-	for (int i = path[c2]; i != c1; i = path[i])
-	{
-		tms += teams[i];
-		cout << i << " ";
-	}
-	cout << endl;
-	cout << dist[c2] << " " << tms;
+	
+	dijkstra(from);
+	cout << way[to] << " " << num[to];
 	return 0;
 }
 /**/
